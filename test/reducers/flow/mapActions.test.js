932150652475a881cb3c08b00b90f12bc33e5d32
Patrick <shantry@hotmail.fr>
2019-12-28 14:41:21 +0100

    Update mapActions.test.js

--- test/reducers/flow/mapActions.test.js
+++ test/reducers/flow/mapActions.test.js
@@ -9,7 +9,7 @@ describe('reducers', () => {
         trackingState = new TrackingState();
       });
 
-      it('apply composable reducer on each resolved actions', () => {
+      it('apply composable reducer on each resolved actions (array)', () => {
         const actions = [];
         const composableReducer = jest.fn().mockImplementation(ts => {
           actions.push(ts.action);
@@ -22,13 +22,39 @@ describe('reducers', () => {
         expect(actions).toEqual(['item1', 'item2', 'item3']);
       });
 
-      it('apply composable reducer on each static actions', () => {
+      it('apply composable reducer on each resolved actions (object)', () => {
+        const actions = [];
+        const composableReducer = jest.fn().mockImplementation(ts => {
+          actions.push(ts.action);
+        });
+        mapActions(
+          () => ({ item1: 'item1', item2: 'item2', item3: 'item3' }),
+          composableReducer
+        )(trackingState);
+        expect(composableReducer).toHaveBeenCalledTimes(3);
+        expect(actions).toEqual(['item1', 'item2', 'item3']);
+      });
+
+      it('apply composable reducer on each static actions (array)', () => {
         const actions = [];
         const composableReducer = jest.fn().mockImplementation(ts => {
           actions.push(ts.action);
         });
         mapActions(
           ['item1', 'item2', 'item3'],
+          composableReducer
+        )(trackingState);
+        expect(composableReducer).toHaveBeenCalledTimes(3);
+        expect(actions).toEqual(['item1', 'item2', 'item3']);
+      });
+
+      it('apply composable reducer on each static actions (object)', () => {
+        const actions = [];
+        const composableReducer = jest.fn().mockImplementation(ts => {
+          actions.push(ts.action);
+        });
+        mapActions(
+          { item1: 'item1', item2: 'item2', item3: 'item3' },
           composableReducer
         )(trackingState);
         expect(composableReducer).toHaveBeenCalledTimes(3);
